43313dd700bfc8bf743af85044d811b15402ef2b
Andrew <andrew.mkal@outlook.com>
2019-07-19 11:52:37 -0500

    Added Next and Previous Buttons

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-import { Card} from 'semantic-ui-react'
+import { Card, Header, Image, Modal } from 'semantic-ui-react'
+
+import axios from 'axios'
 
 const CharacterCard = ({character}) => {
 
-    // const [planet, setPlanet] = useState('')
+    const [planet, setPlanet] = useState('')
 
-    // useEffect(() => {
-    //     axios
-    //         .get(`${character.planet}`)
-    //         .then(response => {
-    //             // setPlanet(response.data.results)
-    //             console.log(response.name)
-    //         })
-    //         .catch(error => {
-    //             console.log('Error connecting to API due to ', error)
-    //         })
-    // }, []);
+    useEffect(() => {
+        axios
+            .get(`${character.homeworld}`)
+            .then(response => {
+                setPlanet(response.data.name)
+            })
+            .catch(error => {
+                console.log('Error connecting to API due to ', error)
+            })
+    }, []);
 
     return(
         <div className='characterCards'>
-            <Card
-                className='characterCard'
-                header={character.name}
-                meta={`Gender: ${character.gender}`}
-                description={`Weight: ${character.mass}`}
-                // extra={extra}
-            />
+            {/* <Card 
+                className='characterCard' 
+                header={character.name} 
+                meta={`Gender: ${character.gender}`} 
+                description={`Weight: ${character.mass}`}> */}
+                    <Modal trigger={<Card 
+                        className='characterCard' 
+                        header={character.name} 
+                        meta={`Gender: ${character.gender}`} 
+                        description={`Weight: ${character.mass}`} 
+                    />}>
+                        <Modal.Header>{character.name}</Modal.Header>
+                        <Modal.Content>
+                        <Modal.Description>
+                            <Header>More Info</Header>
+                            <p>{`Home World: ${planet}`}</p>
+                            <p>{`Birth Year: ${character.birth_year}`}</p>
+                        </Modal.Description>
+                        </Modal.Content>
+                    </Modal>
         </div>
     )
 }
