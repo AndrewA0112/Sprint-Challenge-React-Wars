6429e19b0cf5cf5686cad8c32e091532b3203b0a
Andrew <andrew.mkal@outlook.com>
2019-07-19 10:30:55 -0500

    Added axios get to API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import './App.scss';
+import CharacterCard from './components/CharacterCard';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [charactersList, setCharacterList] = useState([]);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(response => {
+        setCharacterList(response.data.results)
+        console.log(response.data.results)
+      })
+      .catch(error => {
+        console.log('Error connecting to API due to ', error)
+      })
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {
+        charactersList.map((character, i) =>
+          <CharacterCard
+            key = {i}
+            character = {character}
+          />
+      )}
     </div>
   );
 }
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CharacterCard = ({character}) => {
+
+    return(
+        <div>
+            {character.name}
+        </div>
+    )
+}
+
+export default CharacterCard;
